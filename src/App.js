19ff4df15f1faa9e15861915381c6f1a488724e9
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2023-05-19 06:56:49 +0200

    JSON Server is in. REST Client is in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+        // second argument controls whether or not the function will called on second third etc. render
+        // there are 3 options: 
+        // 1: with second argument as [], we call after first render and never again
+        // 2: with no second argument, we call after first render and after every next one
+        // 3: with second argument as [counter], we call after first render and also called after rerenders if 'counter' variable changed
+    }, []);
+
+    // DONT DO THIS:
+    // fetchBooks(); Lektion: 123
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
+        console.log(response);
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
                 //we are updating the object by returning a new one, that takes all the existing properties from the book and then puts a new title in
-                return { ...book, title: newTitle };
+                return { ...book, ...response.data }; // means take all the different properties of that object, take different key value pairs and add them into this new object right here
             }
                 // otherwise if we are mapping through other book, that has a different id, then we just return that book
             return book;
@@ -18,7 +43,9 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
@@ -26,8 +53,13 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
-        const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            // title: title is the same as title alone
+            title
+        });
+
+        const updatedBooks = [...books, response.data];
         setBooks(updatedBooks);
     };
 
